e6e6e98d347d83027d05ef205df3ace2fc9af846
Soeren Hanghoej Moeller <sorenhm0@gmail.com>
2022-12-28 11:12:30 +0100

    refactoring

--- photodember/simulations/fused_silica.py
+++ photodember/simulations/fused_silica.py
@@ -1,260 +1,51 @@
-# %%
+# %% 
 
 from __future__ import annotations
-from dataclasses import dataclass, asdict
-from typing import List, Tuple
 import pathlib
 import json
 import numpy as np
-import scipy as sp
-from numpy.typing import NDArray
-from scipy.special import erf
 
-from photodember.src.constants import SI
-from photodember.src.transport.fdtable import FermiDiracTable
-from photodember.src.transport.fdinterp import interpolate_particle_attributes
-from photodember.src.transport.core import ParticleAttributes
-from photodember.src.transport.simulation import *
+from photodember.simulations.dielectric import DielectricConfig
+from photodember.simulations.simulator import run_simulation
+# from photodember.src.constants import SI
+# from photodember.src.transport.simulation import *
 
 
 # -----------------------------------------------------------------------------
-# Load excitation data 
+# Test simulation....
 
-@dataclass
-class ExcitationData:
-    position: NDArray
-    number_density: NDArray
-    energy_per_particle: NDArray
+def main():
+    conf = DielectricConfig(
+        domain_size = 1e-6,
+        resolution = 2e-9,
+        initial_electron_hole_density = 1e18,
+        excitation_datafile = "photodember/data/Excitation sapphire 2.3 Jcm2.txt",
+        excitation_time_fwhm = 50e-15,
+        excitation_time_zero = 150e-15,
+        fdtable_conduction_band = "photodember/data/SiO2_CB.csv",
+        fdtable_valence_band = "photodember/data/SiO2_VB.csv",
+        carrier_phonon_thermalization_time = 1e-11,
+        electron_hole_thermalization_time = 1e-14,
+        electron_mobility = 3.5e-4,
+        hole_mobility = 5.8e-5,
+        phonon_temperature = 300.0,
+        relative_permittivity = 3.8
+    )
+    simul, initial_state = conf.create_simulation()
+    pde = simul.create_pde()
 
-def load_excitation_data(datafile: str) -> ExcitationData:
-    data = np.loadtxt(datafile)
-    position = data[:, 0] * 1e-6
-    number_density = data[:, 1]
-    energy_per_particle = data[:,2] * SI.e
-    return ExcitationData(position, number_density, energy_per_particle)
+    save_file = "photodember/simulations/fs1.dat"
+    meta_file = save_file.replace(".dat", ".json")
+    if not pathlib.Path(meta_file).exists():
+        with open(save_file.replace(".dat", ".json"), "w") as io:
+            json.dump(conf.to_dict(), io)
+    times = np.linspace(0.0, 1e-12, 1000)
+    result = run_simulation(pde, save_file, initial_state, times)
+    return result
 
-def to_simulation_grid(data: ExcitationData, grid_coordinates: NDArray) -> ExcitationData:
-    number_density = sp.interpolate.interp1d(data.position, data.number_density, kind="cubic")(grid_coordinates)
-    energy_per_particle = sp.interpolate.interp1d(data.position, data.energy_per_particle, kind="cubic")(grid_coordinates)
-    return ExcitationData(grid_coordinates, number_density, energy_per_particle)
+# %%
 
-def particle_temperatures(energy_per_particle: NDArray, particle_masses: List[float]) -> List[NDArray]:
-    inverse_masses = np.divide(1.0, particle_masses)
-    temperature = energy_per_particle * (2/3) / SI.kB
-    def particle_temperature(inverse_mass: float) -> NDArray:
-        return temperature * (inverse_mass / np.sum(inverse_masses))
-    return [particle_temperature(inv_mass) for inv_mass in inverse_masses]
-
-
-# -----------------------------------------------------------------------------
-# Load particle data
-
-@dataclass
-class ParticleData:
-    charge: float
-    mobility: float
-    source: str
-
-    @property
-    def mass(self) -> float:
-        return FermiDiracTable.load_meta(self.source.replace(".csv", ".json"))["relativeMass"] * SI.m_e
-
-def set_mobility(table: FermiDiracTable, meta: dict, new_value: float) -> Tuple[FermiDiracTable, dict]:
-    relaxtime = meta["relaxationTime"]
-    old_value = SI.e * relaxtime / (meta["relativeMass"] * SI.m_e)
-    scale = new_value / old_value
-    new_table = table.scale_mobility(scale)
-    new_meta = dict(meta)
-    new_meta["relaxationTime"] = meta["relaxationTime"] * scale
-    return new_table, new_meta
-
-def load_particle_attributes(data: ParticleData) -> ParticleAttributes:
-    table = FermiDiracTable.load_data(data.source)
-    meta = FermiDiracTable.load_meta(data.source.replace(".csv", ".json"))
-    new_table, _ = set_mobility(table, meta, data.mobility)
-    return interpolate_particle_attributes(new_table, data.charge, SI.kB)
-
-
-# -----------------------------------------------------------------------------
-# Fused silica simulation
-
-def heat_capacity_from_state(attrs: ParticleAttributes, state: ParticleState):
-    return heat_capacity(np.zeros(state.grid_points), attrs.number_density, attrs.energy_density, state.temperature, state.red_chemical_potential)
-
-def create_source_term(N: int, particles: List[ParticleAttributes], final_density: NDArray, tfwhm: float, t0: float, geh: float, gph: float, Tph: float) -> OdeFunction:
-    assert len(particles) == 2
-    indexer = StateIndexer(N, 2)
-    elec_attrs, hole_attrs = particles
-    elec = indexer.particle(0)
-    hole = indexer.particle(1)
-    out = np.zeros(indexer.length)
-
-    A = np.sqrt(4*np.log(2)/np.pi)/tfwhm
-    G = np.zeros(indexer.grid_points)
-    def d_dt(t):
-        G[:] = final_density * A * np.exp(-4*np.log(2)*((t-t0)/tfwhm)**2)
-        return G
-
-    def rise(t):
-        return .5*(1 + erf(np.sqrt(4*np.log(2))*(t-t0)/tfwhm))
-
-    def S(t, y):
-        out.fill(0.0)
-        G = d_dt(t)
-        out[elec.number_density] += G
-        out[hole.number_density] += G
-
-        Te, etae = y[elec.temperature], y[elec.red_chemical_potential]
-        Th, etah = y[hole.temperature], y[hole.red_chemical_potential]
-
-        dUe_dNe = elec_attrs.energy_density.deta(Te, etae) / elec_attrs.number_density.deta(Te, etae) # type: ignore
-        dUh_dNh = hole_attrs.energy_density.deta(Th, etah) / hole_attrs.number_density.deta(Th, etah) # type: ignore
-        
-        # if t < t0 + 1.5*tfwhm:
-        #     out[elec.energy_density] += G*dUe_dNe #- geh*(Te - Th) - gph*(Te - Tph)
-        #     out[hole.energy_density] += G*dUh_dNh #- geh*(Th - Te) - gph*(Th - Tph)
-        # else:
-        r = rise(t)
-        out[elec.energy_density] += G*dUe_dNe - r*(geh*(Te - Th) + gph*(Te - Tph))
-        out[hole.energy_density] += G*dUh_dNh - r*(geh*(Th - Te) + gph*(Th - Tph))
-        return out
-    return S
-
-
-@dataclass
-class FusedSilicaConfig:
-    domain_size: float
-    resolution: float
-    initial_electron_hole_density: float
-    excitation_datafile: str
-    excitation_time_fwhm: float
-    excitation_time_zero: float
-    fdtable_conduction_band: str
-    fdtable_valence_band: str
-    electron_mobility: float
-    hole_mobility: float
-    electron_hole_thermalization_time: float
-    carrier_phonon_thermalization_time: float
-    phonon_temperature: float
-    relative_permittivity: float
-
-    @property
-    def grid(self) -> NDArray:
-        return np.arange(0, self.domain_size, step=self.resolution)
-
-    @property
-    def electron_data(self) -> ParticleData:
-        return ParticleData(-SI.e, self.electron_mobility, self.fdtable_conduction_band)
-
-    @property
-    def hole_data(self) -> ParticleData:
-        return ParticleData(SI.e, self.hole_mobility, self.fdtable_valence_band)
-
-    @property
-    def particle_attributes(self):
-        return [load_particle_attributes(self.electron_data),
-                load_particle_attributes(self.hole_data)]
-
-    def create_simulation(self) -> Tuple[Simulation, SimulationState]:
-        x = self.grid
-        N = len(x)
-
-        particle_attrs = self.particle_attributes
-
-        excitation_data = to_simulation_grid(load_excitation_data(self.excitation_datafile), x)
-        electron_hole_density = self.initial_electron_hole_density * np.ones_like(x)
-        masses = [self.electron_data.mass, self.hole_data.mass]
-        temperatures = particle_temperatures(excitation_data.energy_per_particle, masses)
-        excited_final_state = [ # state immediately after excitation in the absence of transport (i.e. max density)
-            ParticleState.create(excitation_data.number_density, temperatures[0], particle_attrs[0]),
-            ParticleState.create(excitation_data.number_density, temperatures[1], particle_attrs[1])
-        ]
-
-        Ceh = np.array([heat_capacity_from_state(attrs, state) for (attrs, state) in zip(particle_attrs, excited_final_state)]).sum(axis=0)
-        geh = np.average(Ceh) / self.electron_hole_thermalization_time
-        gph = np.average(Ceh) / self.carrier_phonon_thermalization_time
-        
-        source_fn = create_source_term(N, particle_attrs, excitation_data.number_density, self.excitation_time_fwhm, self.excitation_time_zero, geh, gph, self.phonon_temperature)
-        simul = Simulation(N, self.resolution, SI, self.relative_permittivity, particle_attrs, source_fn)
-        initial_electric_field = np.zeros_like(x)
-        initial_state = SimulationState.create(
-                   [ParticleState.create(electron_hole_density, temperatures[0], particle_attrs[0]),
-                    ParticleState.create(electron_hole_density, temperatures[1], particle_attrs[1])],
-                    initial_electric_field)
-        return simul, initial_state
-
-    def to_dict(self) -> dict:
-        return asdict(self)
-
-    @staticmethod
-    def from_dict(config: dict) -> FusedSilicaConfig:
-        return FusedSilicaConfig(**config)
-
-
-# %% 
-
-import gc
-
-def advance(pde: OdeFunction, init_state: SimulationState, ti: float, dt: float) -> SimulationState:
-    gc.collect() # sometimes memory consumption keeps rising, dunno why... this helps...
-    return solve_pde(pde, init_state, [ti, ti + dt])[-1]
-
-def save_state_in(f, state: SimulationState, t: float):
-    s = np.append([t], state.array).astype(np.float64).tobytes()
-    f.write(s)
-
-def simulation_step(f, pde: OdeFunction, init_state: SimulationState, ti: float, tf: float):
-    new_state = advance(pde, init_state, ti, tf-ti)
-    save_state_in(f, new_state, tf)
-    return new_state
-
-def read_simulation_file(filename: str, initial_state: SimulationState) -> Tuple[List[float], List[SimulationState]]:
-    t = []
-    states = []
-    chunksize = len(initial_state.array) + 1
-    with open(filename, "rb") as io:
-        content = np.frombuffer(io.read(), dtype=np.float64)
-        start, stop = 0, chunksize
-        while stop <= len(content):
-            section = content[start:stop]
-            t.append(section[0])
-            state = SimulationState(section[1:], initial_state.grid_points, initial_state.number_particles)
-            states.append(state)
-            start, stop = stop, stop + chunksize
-    return t, states
-
-def run_simulation(pde: OdeFunction, save_file: str, init_state: SimulationState, times: ArrayLike):
-    t = list(times)
-    ti = t.pop(0)
-    # Scan file for existing entries
-    if pathlib.Path(save_file).exists():
-        read_t, read_states = read_simulation_file(save_file, init_state)
-        if len(read_t) > 0:
-            if ti == read_t[0] and all(np.isclose(init_state.array, read_states[0].array)):
-                ti = read_t[-1]
-                init_state = read_states[-1]
-                t = list(filter(lambda t: t > ti, times))
-            else:
-                raise ValueError("Simulation file already exists but saved states differ")
-    else:
-        with open(save_file, "wb") as f:
-            save_state_in(f, init_state, ti)
-    # Loop
-    states = [init_state]
-    while len(t) > 0:
-        tf = t.pop(0)
-        print(f"Advancing state from t={ti} to t={tf}...")
-        with open(save_file, "ab") as f:
-            new_state = simulation_step(f, pde, init_state, ti, tf)
-        states.append(new_state)
-        ti = tf
-        init_state = new_state
-    return states
-
-# -----------------------------------------------------------------------------
-# Simulator
-
-conf = FusedSilicaConfig(
+conf = DielectricConfig(
     domain_size = 1e-6,
     resolution = 2e-9,
     initial_electron_hole_density = 1e18,
@@ -273,10 +64,96 @@ conf = FusedSilicaConfig(
 simul, initial_state = conf.create_simulation()
 pde = simul.create_pde()
 
-save_file = "photodember/simulations/fs1.dat"
-meta_file = save_file.replace(".dat", ".json")
-if not pathlib.Path(meta_file).exists():
-    with open(save_file.replace(".dat", ".json"), "w") as io:
-        json.dump(conf.to_dict(), io)
-times = np.linspace(0.0, 1e-12, 1000)
-result = run_simulation(pde, save_file, initial_state, times)
+# # %%
+
+# save_file = "photodember/simulations/fs1_source_term_with_T_rise_2.dat"
+# meta_file = save_file.replace(".dat", ".json")
+# if not pathlib.Path(meta_file).exists():
+#     with open(save_file.replace(".dat", ".json"), "w") as io:
+#         json.dump(conf.to_dict(), io)
+# times = np.linspace(0.0, 1e-12, 1000)
+# result = run_simulation(pde, save_file, initial_state, times)
+
+
+# # %%
+# import matplotlib.pyplot as plt
+
+# tfwhm = 50e-15
+# t, dt = np.linspace(-3*tfwhm, 3*tfwhm, 1000, retstep=True)
+# r = lambda t: .5 * (1 + erf(np.sqrt(4*np.log(2))*t/tfwhm))
+# A = np.sqrt(4*np.log(2)/np.pi)/tfwhm
+# drdt = lambda t: A * np.exp(-4*np.log(2)*(t/tfwhm)**2)
+# N = 1e28 * r(t)
+# T = (7000-300.0) * r(t) + 300.0
+
+# electron = simul.particles[0]
+
+# def dHdt(Ti, Tf, Nf, t, T, eta):
+#     dN = Nf * drdt(t)
+#     dT = (Tf - Ti) * drdt(t)
+#     U, N = electron.energy_density, electron.number_density
+#     dH = (U.dT(T, eta) - U.deta(T, eta) * N.dT(T, eta)/N.deta(T, eta)) * dT + U.deta(T, eta)/N.deta(T, eta) * dN
+#     return dH
+
+# eta = [solve_reduced_chemical_potential(electron.number_density, Ti, Ni, [-100, 100]) for Ti, Ni in zip(T, N)]
+# dH = dHdt(300.0, 7000.0, 1e28, t, T, eta)
+
+# G = lambda t: 1e28 * drdt(t)
+# H = lambda t: electron.number_density
+
+# # %%
+
+# Nt = electron.number_density(T, eta)
+# Ht = electron.energy_density(T, eta)
+
+# # plt.plot(t, Nt, "b-")
+# # plt.twinx()
+# plt.plot(t, Ht, "r-")
+# plt.plot(t, np.cumsum(dH*dt))
+
+# # %%
+# E = np.array([s.electric_field for s in result])
+# Ne = np.array([s.number_density[0] for s in result])
+# Nh = np.array([s.number_density[1] for s in result])
+# Te = np.array([s.temperature[0] for s in result])
+# Th = np.array([s.temperature[1] for s in result])
+# # %%
+
+# import matplotlib.pyplot as plt
+# from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+
+# plt.rcParams["font.family"] = "Arial"
+# plt.rcParams["axes.labelsize"] = "large"
+# plt.rcParams["xtick.direction"] = "in"
+# plt.rcParams["ytick.direction"] = "in"
+
+# # %%
+
+# plt.figure(figsize=(5,5.5))
+# t = np.array(times) * 1e15
+
+# plt.subplot(2,1,1)
+# plt.plot(t, Te[:,0], "r-", label="electron")
+# plt.plot(t, Th[:,0], "b-", label="hole")
+# # plt.plot(t, Te[:,-1], "r--")#, label="electron")
+# # plt.plot(t, Th[:,-1], "b--")#, label="hole")
+# plt.xlim([0, 1e3])
+# plt.gca().yaxis.set_major_locator(MultipleLocator(300))
+# plt.gca().xaxis.set_major_locator(MultipleLocator(100))
+# plt.legend(frameon=False)
+# plt.ylim([300, 3300])
+# plt.ylabel("Surface temperature (K)")
+
+# plt.subplot(2,1,2)
+# plt.plot(t, E[:,1]*.5*1e-7, "g-")
+# plt.xlim([0, 1e3])
+# plt.gca().xaxis.set_major_locator(MultipleLocator(100))
+# plt.xlabel("Time (fs)")
+# plt.ylabel(r"Surface field (10$^7$ V/m)")
+# plt.ylim([0, 1.6])
+
+# plt.tight_layout()
+# plt.savefig("Temperature.png", dpi=300, facecolor="w")
+# # %%
+
+# %%
---
+++ photodember/simulations/plots.py
@@ -0,0 +1,338 @@
+# %%
+import json
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+from photodember.simulations.fused_silica import *
+
+# Pimp my plot
+# -----------------------------------------------------------------------------
+# %%
+
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+
+plt.rcParams["font.family"] = "Arial"
+plt.rcParams["axes.labelsize"] = "large"
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rcParams['animation.ffmpeg_path'] = "C:/ffmpeg/bin/ffmpeg.exe"
+
+# Load simulation
+# -----------------------------------------------------------------------------
+# %%
+
+simulfile = "photodember/simulations/fs1_source_term_with_T_rise_2.dat"
+metafile = simulfile.replace(".dat", ".json")
+
+with open(metafile, "r") as io:
+    simulconf = DielectricConfig.from_dict(json.load(io))
+
+init_state = simulconf.initial_state
+simul, _ = simulconf.create_simulation()
+t, states = read_simulation_file(simulfile, init_state)
+
+# Prepare variables
+# -----------------------------------------------------------------------------
+# %%
+
+x = simulconf.grid
+t_fs = np.array(t) * 1e15
+electron_states = [st.particle(0) for st in states]
+hole_states = [st.particle(1) for st in states]
+electric_field = np.array([st.electric_field for st in states])
+
+peak_density_tzero = np.argmax([st.number_density[0] for st in electron_states])
+
+# %% Centroid
+
+t_s = np.array(t)
+electron_density = np.array([st.number_density[0] for st in states])
+
+exprep0 = lambda x, N: np.repeat(np.expand_dims(x, 0), N, 0)
+exprep1 = lambda x, N: np.repeat(np.expand_dims(x, 1), N, 1)
+
+X = exprep0(x, len(t))
+Pr = electron_density / exprep1(np.sum(electron_density, axis=1), len(x))
+mean_X = np.sum(X * Pr, axis=1)
+var_X = np.sum((X - exprep1(mean_X, len(x)))**2 * Pr, axis=1)
+
+ids = np.array(t) > conf.excitation_time_zero
+
+plt.figure(figsize=(5,4.5))
+# plt.plot(t_fs[ids], (mean_X[ids]-mean_X[ids][0]) * 1e9)
+plt.plot(t_fs[ids], np.sqrt(var_X[ids]) * 1e9)
+plt.xlabel(r"Time, $t$ (fs)")
+plt.ylabel(r"Centroid, $\Delta x$ (nm)")
+# plt.ylim([0, 2.1])
+plt.xlim([0, 5000])
+# plt.xscale("log")
+# plt.yscale("log")
+plt.tight_layout()
+# np.mean(electron_density)
+
+# %%
+
+surface_electric_field = [st.electric_field[1]*.5 for st in states]
+
+plt.plot(t_fs, surface_electric_field, "g-")
+plt.xlim([0, 1000])
+plt.ylim([0, 1.6e7])
+
+# FIGURE 1
+# -----------------------------------------------------------------------------
+# %%
+
+x_nm = x * 1e9
+ts = [0.0, 10e-15, 100e-15, 1e-12]
+Esim = np.copy(electric_field)
+ids = [np.argmin(np.abs(np.array(t)-ti)) for ti in ts]
+lines = ["-", "--", "-.", ":"]
+
+fig = plt.figure(figsize=(4.5,5.5), dpi=125)
+plt.subplot(2,1,1)
+plt.plot(x_nm, electron_states[peak_density_tzero].number_density * 1e-28, "k-")
+plt.xlim([0, 1000])
+plt.ylim([0, 1])
+plt.xlabel(r"Coordinate, $x$ (nm)")
+plt.ylabel(r"Density, $N$ (10$^{28}$ m$^{-3}$)")
+plt.gca().yaxis.set_major_locator(MultipleLocator(0.2))
+plt.gca().yaxis.set_minor_locator(MultipleLocator(0.04))
+plt.annotate(r"$N$", xy=(30, 0.7), fontsize="large", color="k")
+plt.annotate(r"$T_e$", xy=(200, 0.88), fontsize="large", color="b")
+plt.annotate(r"$T_h$", xy=(180, 0.65), fontsize="large", color="r")
+ax = plt.twinx()
+plt.plot(x_nm, electron_states[peak_density_tzero].temperature, "b-")
+plt.plot(x_nm, hole_states[peak_density_tzero].temperature, "r-")
+# plt.plot(x_nm, electron_states[peak_density_tzero].temperature, "b-")
+# plt.plot(x_nm, hole_states[peak_density_tzero].temperature, "r--")
+plt.ylim([2300, 4000])
+plt.ylabel(r"Temperature, $T_i$ (K)")
+ax.yaxis.set_major_locator(MultipleLocator(300))
+ax.yaxis.set_minor_locator(MultipleLocator(100))
+
+plt.subplot(2,1,2)
+t_fs = np.array(t) * 1e15
+plt.plot(t_fs, .5*(Esim[:,0]+Esim[:,1])*1e-7, "g-")
+plt.fill_between(t_fs, 1.3*np.exp(-4*np.log(2)*((np.array(t)-conf.excitation_time_zero)/conf.excitation_time_fwhm)**2), lw=1, ls="-", color="k", hatch='///', fc="k", alpha=0.33, zorder=-3)
+# plt.xscale("log")
+plt.xlabel(r"Time, $t$ (fs)")
+plt.ylabel(r"Electric field, $E_D$ (10$^7$ V m$^{-1}$)")
+plt.ylim([0, 1.6])
+plt.gca().yaxis.set_minor_locator(MultipleLocator(0.05))
+plt.annotate(r"$E_D$", xy=(270, 0.7), fontsize="large", color="g")
+plt.annotate(r"$T_e$", xy=(200, 1.35), fontsize="large", color="b")
+plt.annotate(r"$T_h$", xy=(230, 1.1), fontsize="large", color="r")
+
+plt.twinx()
+Te_surf = [st.temperature[0] for st in electron_states]
+Th_surf = [st.temperature[0] for st in hole_states]
+plt.plot(t_fs, Te_surf, "b-")
+plt.plot(t_fs, Th_surf, "r-")
+plt.ylabel(r"Temperature, $T_i$ (K)")
+plt.xlim([0, 1e3])
+plt.ylim([0, 3500])
+plt.gca().xaxis.set_minor_locator(MultipleLocator(25))
+plt.gca().yaxis.set_minor_locator(MultipleLocator(100))
+plt.gca().yaxis.set_major_locator(MultipleLocator(500))
+
+plt.tight_layout()
+# fig.savefig("Fig2.png", dpi=300, facecolor="w")
+
+
+# Semi-classical equation of motion
+# -----------------------------------------------------------------------------
+# %% Semi-classical equation of motion
+
+dt = t[1]-t[0]
+# E = np.array([simul.ambipolar_electric_field(st) for st in states])
+k = -SI.e/SI.hbar * np.cumsum(electric_field*dt, axis=0)
+
+a = 4.9e-10 # https://link.springer.com/content/pdf/10.1007/BF00552441.pdf
+kBz = np.pi/a
+
+x0 = 0.0 # nm
+ix = np.argmin(np.abs(x - x0))
+
+plt.figure(figsize=(5,4), dpi=150)
+plt.plot(x_nm, -k[400,:]/kBz, "g-", label="t = 0.2 ps")
+plt.plot(x_nm, -k[1000,:]/kBz, "c-", label="t = 0.6 ps")
+plt.plot(x_nm, -k[-1,:]/kBz, "m-", label="t = 1.0 ps")
+plt.legend(frameon=False)
+plt.ylabel(r"Momentum, $\Delta k/k_{\mathrm{BZ}}$")
+plt.xlabel("Coordinate, x (nm)")
+plt.xlim([0, 500])
+# plt.ylim([0, 0.07])
+plt.tight_layout()
+
+
+# Band structure
+# -----------------------------------------------------------------------------
+# %%
+
+def energy_vs_k(alpha, m, kx, ky, kz):
+    k = np.sqrt(kx**2 + ky**2 + kz**2)
+    q = SI.hbar*k/np.sqrt(m)
+    return (-1 + np.sqrt(1 + 2*alpha*q**2))/(2*alpha)
+
+def effective_mass(m, alpha, kx):
+    q = SI.hbar * kx / np.sqrt(m)
+    return m * (1 + 2*alpha*q**2)**(3/2)
+
+ky = 0.0; kz = 0.0
+kx = np.linspace(-0.8, 0.8, 200) * kBz
+alpha = 1.0/SI.e
+mc = 0.5*SI.m_e
+ek = energy_vs_k(alpha, mc, kx, ky, kz)
+
+tau_eph = 100e-15
+dt = t[1]-t[0]
+dt_array = np.gradient(t)
+conv = np.exp(-np.array(t)/tau_eph) * dt_array
+electric_field_t_integral = np.array([np.convolve(conv, electric_field[:,i])[:len(t)] for i in range(0, electric_field.shape[1])])
+
+
+# Helmholtz....
+# -----------------------------------------------------------------------------
+# %%
+
+from photodember.src.optics.helmholtz import solve_stack, Stack, Layer
+from photodember.src.optics.optical_models import Drude
+from scipy.interpolate import interp1d
+
+eps_v = 3.11
+n_val = 3.2e29
+mc = 0.5 * SI.m_e
+probe_om = 800/700 * 2.35e15
+alpha = 0.2 / SI.e
+xgrid = np.copy(x)
+dx = xgrid[1]-xgrid[0]
+
+def create_eps_from(om, density, mass, gam):
+    def eps(x):
+        nx = density(x)
+        om_p = np.sqrt(SI.e**2*nx/(SI.eps_0*mass(x)))
+        return eps_v - (eps_v - 1)*(nx/n_val) - om_p**2/(om*(om + 1j*gam(x)))
+    return eps
+
+state_index = 100
+
+def solve_helmholtz(alpha, gam, state_index):
+    state = electron_states[state_index]
+    k_z = interp1d(xgrid, SI.e * electric_field_t_integral[:, state_index] / SI.hbar, kind="cubic")
+    mz = lambda x: effective_mass(mc, alpha, k_z(x))
+    gx = lambda x: gam
+
+    eps_x = create_eps_from(probe_om, interp1d(xgrid, state.number_density), lambda x: mc, gx)
+    eps_z = create_eps_from(probe_om, interp1d(xgrid, state.number_density), mz, gx)
+    stack_s = Stack([
+        Layer.create(xgrid[-1]-dx, eps_x), 
+        Layer.create(np.inf, lambda _: eps_v + 0j)])
+    stack_p = Stack([
+        Layer.create(xgrid[-1]-dx, eps_z), 
+        Layer.create(np.inf, lambda _: eps_v + 0j)])
+
+    ss = solve_stack(stack_s, probe_om, 60.0, "s")
+    sp = solve_stack(stack_p, probe_om, 60.0, "p")
+    return ss, sp
+
+state_inds = list(range(0, 200, 50)) + list(range(210, 390, 10)) + list(range(400, 1000, 50)) + [len(t)-1]
+alphas_int = [0.1, 0.2, 0.4]
+alphas = np.array(alphas_int) / SI.e
+gam = 1e15
+hhsols = [[solve_helmholtz(alpha, gam, i) for i in state_inds] for alpha in alphas]
+
+# THz....
+# -----------------------------------------------------------------------------
+# %%
+
+skindepth_THz = 1e-9
+dx = x[1]-x[0]
+THz_averager = np.repeat(np.expand_dims(np.exp(-(x+0.5*dx)/skindepth_THz) * dx, 0), len(t), axis=0)
+J_tot = np.array([sum(simul.charge_current_density(state)) for state in states])
+J_THz_ = CubicSpline(t, np.sum(J_tot * THz_averager, axis=1))#, k=3, s=len(t))
+J_THz = J_THz_(t)
+# dJdt = np.gradient(J_THz, t, axis=0)
+# d2Jdt2 = np.gradient(dJdt, t, axis=0)
+dJdt = J_THz_(t, 1)
+d2Jdt2 = J_THz_(t, 2)
+Ens = SI.mu_0/(6*np.pi*SI.c_0) * d2Jdt2
+Prad = -Ens * J_THz
+
+plt.plot(t_fs, Prad)
+# plt.xlim([0, 1500])
+
+
+# FIGURE 3
+# -----------------------------------------------------------------------------
+# %%
+
+delta_k = SI.e * electric_field_t_integral[1,:] / SI.hbar / kBz
+Ne = np.array([st.number_density for st in electron_states]).T
+skindepth = 40 # nm; good until about 40 nm...
+p = np.exp(-x_nm/skindepth); p /= np.sum(p)
+P = np.repeat(np.expand_dims(p, 1), len(t), axis=1)
+t_offset = np.array(t) - simulconf.excitation_time_zero #- t[peak_density_tzero]
+
+
+fig = plt.figure(figsize=(4.5,9.0), dpi=125)
+plt.subplot(4,1,1)
+kxt = np.sum(delta_k * P, axis=0)
+plt.plot(np.array(t_offset)*1e15, kxt, "k-")
+plt.xlim([-150, 850])
+plt.ylim([0, 0.4])
+# plt.xscale("log")
+plt.ylabel(r"Momentum, $\Delta k / k_{\mathrm{BZ}}$")
+plt.xlabel(r"Time, $t$ (fs)")
+
+plt.subplot(4,1,2)
+ls = ["c-", "m--", "y-."]
+for i, alpha in enumerate(alphas_int):
+    mxt = np.interp(kxt*kBz, kx, effective_mass(mc, alpha/SI.e, kx)) / mc
+    # mx = effective_mass(alpha/SI.e, kx) / mc
+    plt.plot(np.array(t_offset)*1e15, mxt, ls[i])
+plt.xlabel("")
+plt.xlim([0, 800])
+plt.ylim([1, 2.25])
+plt.annotate(r"$\alpha$ = 0.1 eV$^{-1}$", xy=(140, 1.3), color="c")
+plt.annotate(r"$\alpha$ = 0.2 eV$^{-1}$", xy=(170, 1.6), color="m")
+plt.annotate(r"$\alpha$ = 0.4 eV$^{-1}$", xy=(270, 1.9), color="y")
+plt.xlabel(r"Time, $t$ (fs)")
+plt.ylabel(r"Effective mass, $m / m_c$")
+
+Rps = []
+for hhsola in hhsols:
+    Rp_ = [elem[1].R for elem in hhsola]
+    Rps.append(Rp_)
+
+plt.subplot(4,1,3)
+# Reflection, transmission
+plt.plot(t_offset[state_inds]*1e15, Rps[0], "c-")
+plt.plot(t_offset[state_inds]*1e15, Rps[1], "m--")
+plt.plot(t_offset[state_inds]*1e15, Rps[2], "y-.")
+plt.annotate(r"$\alpha$ = 0.1 eV$^{-1}$", xy=(250, 0.13), color="c")
+plt.annotate(r"$\alpha$ = 0.2 eV$^{-1}$", xy=(300, 0.06), color="m")
+plt.annotate(r"$\alpha$ = 0.4 eV$^{-1}$", xy=(550, 0.02), color="y")
+plt.ylabel("Reflection, $R_p$")
+plt.xlabel("Time, $t$ (fs)")
+# plt.gca().xaxis.set_major_locator(MultipleLocator(50))
+plt.gca().xaxis.set_major_locator(MultipleLocator(100))
+# plt.gca().xaxis.set_minor_locator(MultipleLocator(50))
+plt.xlim([-150, 850])
+plt.ylim([0, 0.25])
+
+# THz
+plt.subplot(4,1,4)
+plt.plot(t_offset*1e15, J_THz, "k-")
+plt.xlim([-150.0, 850])
+plt.gca().xaxis.set_major_locator(MultipleLocator(100))
+plt.ylabel(r"Current, $J_{\mathrm{THz}}$ (J/m$^2$)")
+plt.xlabel(r"Time, $t$ (fs)")
+# ax = plt.twinx()
+# plt.plot(t_offset*1e15, electric_field[:,1], "g--", zorder=-10)
+# plt.ylabel("Electric field")
+
+plt.tight_layout()
+# fig.savefig("Fig3.png", dpi=300)
+# %%

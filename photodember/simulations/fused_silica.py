7710ba7f951bdb576e74ef2fa9b6a2fd863efbb8
Søren Hanghøj Møller <sorenhm0@gmail.com>
2023-01-12 16:07:22 +0100

    clean up

--- photodember/simulations/fused_silica.py
+++ photodember/simulations/fused_silica.py
@@ -1,21 +1,46 @@
 # %%
 
 from __future__ import annotations
+from dataclasses import replace
 import pathlib
 import json
 import numpy as np
 
-from photodember.simulations.simulator import run_simulation, DielectricConfig
+from photodember.simulations.core import run_simulation, SimulationConfig
 
 # %%
 
-conf = DielectricConfig(
+
+def generate_excitation_data_simple_exp(
+    surface_density: float,
+    kinetic_energy_eV: float,
+    length_scale_mu: float,
+    x_mu: np.ndarray,
+):
+    data = np.zeros((len(x_mu), 3))
+    data[:, 0] = np.copy(x_mu)
+    data[:, 1] = surface_density * np.exp(-x_mu / length_scale_mu)
+    data[:, 2] = kinetic_energy_eV * np.ones_like(x_mu)
+    return data
+
+
+length_scale_nm = 40
+np.savetxt(
+    f"photodember/data/Excitation FS SimpleExp {length_scale_nm} nm.txt",
+    generate_excitation_data_simple_exp(
+        1.45e28, 1.075, length_scale_nm * 1e-3, np.linspace(0, 2, 1000)
+    ),
+)
+
+# %%
+
+default_config = SimulationConfig(
     domain_size=1e-6,
-    resolution=2e-9,
+    resolution=1e-9,
     initial_electron_hole_density=1e18,
-    excitation_datafile="photodember/data/Excitation sapphire 2.3 Jcm2.txt",
-    excitation_time_fwhm=50e-15,
-    excitation_time_zero=150e-15,
+    excitation_datafile=r"photodember\data\Excitation FS SimpleExp 40 nm.txt",
+    excitation_time_fwhm=35e-15,
+    excitation_time_zero=105e-15,
     fdtable_conduction_band="photodember/data/SiO2_CB.csv",
     fdtable_valence_band="photodember/data/SiO2_VB.csv",
     carrier_phonon_thermalization_time=1e-11,
@@ -25,99 +50,23 @@ conf = DielectricConfig(
     phonon_temperature=300.0,
     relative_permittivity=3.8,
 )
-simul, initial_state = conf.create_simulation()
-pde = simul.create_pde()
+
 
 # %%
 
-save_file = "photodember/simulations/fs1_source_term_with_T_rise_4.dat"
-meta_file = save_file.replace(".dat", ".json")
-if not pathlib.Path(meta_file).exists():
-    with open(save_file.replace(".dat", ".json"), "w") as io:
-        json.dump(conf.to_dict(), io)
-times = np.linspace(0.0, 1e-12, 1000)
-result = run_simulation(pde, save_file, initial_state, times)
+mobility_scaling = [1.0]  # [0.1, 1.0, 10.0]
+for mu_scaling in mobility_scaling:
+    save_file = f"photodember/simulations/fs1_source_term_with_T_rise_1nm_35fs_muscale-{mu_scaling}_SimpleExp-{length_scale_nm}nm.dat"
+    meta_file = save_file.replace(".dat", ".json")
 
+    mu_e = default_config.electron_mobility * mu_scaling
+    mu_h = default_config.hole_mobility * mu_scaling
+    conf = replace(default_config, electron_mobility=mu_e, hole_mobility=mu_h)
+    simul, initial_state = conf.create_simulation()
+    pde = simul.create_pde()
 
-# # %%
-# import matplotlib.pyplot as plt
-
-# tfwhm = 50e-15
-# t, dt = np.linspace(-3*tfwhm, 3*tfwhm, 1000, retstep=True)
-# r = lambda t: .5 * (1 + erf(np.sqrt(4*np.log(2))*t/tfwhm))
-# A = np.sqrt(4*np.log(2)/np.pi)/tfwhm
-# drdt = lambda t: A * np.exp(-4*np.log(2)*(t/tfwhm)**2)
-# N = 1e28 * r(t)
-# T = (7000-300.0) * r(t) + 300.0
-
-# electron = simul.particles[0]
-
-# def dHdt(Ti, Tf, Nf, t, T, eta):
-#     dN = Nf * drdt(t)
-#     dT = (Tf - Ti) * drdt(t)
-#     U, N = electron.energy_density, electron.number_density
-#     dH = (U.dT(T, eta) - U.deta(T, eta) * N.dT(T, eta)/N.deta(T, eta)) * dT + U.deta(T, eta)/N.deta(T, eta) * dN
-#     return dH
-
-# eta = [solve_reduced_chemical_potential(electron.number_density, Ti, Ni, [-100, 100]) for Ti, Ni in zip(T, N)]
-# dH = dHdt(300.0, 7000.0, 1e28, t, T, eta)
-
-# G = lambda t: 1e28 * drdt(t)
-# H = lambda t: electron.number_density
-
-# # %%
-
-# Nt = electron.number_density(T, eta)
-# Ht = electron.energy_density(T, eta)
-
-# # plt.plot(t, Nt, "b-")
-# # plt.twinx()
-# plt.plot(t, Ht, "r-")
-# plt.plot(t, np.cumsum(dH*dt))
-
-# # %%
-# E = np.array([s.electric_field for s in result])
-# Ne = np.array([s.number_density[0] for s in result])
-# Nh = np.array([s.number_density[1] for s in result])
-# Te = np.array([s.temperature[0] for s in result])
-# Th = np.array([s.temperature[1] for s in result])
-# # %%
-
-# import matplotlib.pyplot as plt
-# from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-
-# plt.rcParams["font.family"] = "Arial"
-# plt.rcParams["axes.labelsize"] = "large"
-# plt.rcParams["xtick.direction"] = "in"
-# plt.rcParams["ytick.direction"] = "in"
-
-# # %%
-
-# plt.figure(figsize=(5,5.5))
-# t = np.array(times) * 1e15
-
-# plt.subplot(2,1,1)
-# plt.plot(t, Te[:,0], "r-", label="electron")
-# plt.plot(t, Th[:,0], "b-", label="hole")
-# # plt.plot(t, Te[:,-1], "r--")#, label="electron")
-# # plt.plot(t, Th[:,-1], "b--")#, label="hole")
-# plt.xlim([0, 1e3])
-# plt.gca().yaxis.set_major_locator(MultipleLocator(300))
-# plt.gca().xaxis.set_major_locator(MultipleLocator(100))
-# plt.legend(frameon=False)
-# plt.ylim([300, 3300])
-# plt.ylabel("Surface temperature (K)")
-
-# plt.subplot(2,1,2)
-# plt.plot(t, E[:,1]*.5*1e-7, "g-")
-# plt.xlim([0, 1e3])
-# plt.gca().xaxis.set_major_locator(MultipleLocator(100))
-# plt.xlabel("Time (fs)")
-# plt.ylabel(r"Surface field (10$^7$ V/m)")
-# plt.ylim([0, 1.6])
-
-# plt.tight_layout()
-# plt.savefig("Temperature.png", dpi=300, facecolor="w")
-# # %%
-
-# %%
+    if not pathlib.Path(meta_file).exists():
+        with open(save_file.replace(".dat", ".json"), "w") as io:
+            json.dump(conf.to_dict(), io)
+    times = np.linspace(0.0, 2.0e-12, 1000)  # , np.linspace(1.001e-12, 5e-12, 500))
+    result = run_simulation(pde, save_file, initial_state, times)
